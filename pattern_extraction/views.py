6cb0610004a274999abb46e58488dea4177d18e1
ChanYeongShin <cyshin@yonsei.ac.kr>
2019-11-18 20:14:39 +0900

    [ADDED] Category Extraction API

--- pattern_extraction/views.py
+++ pattern_extraction/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+import requests
+from PIL import Image
 
-# Create your views here.
+
+class PatternExtractionAPIView(GenericAPIView):
+
+    def post(self, request, *args, **kwargs):
+        detected_image_url = request.data['url']
+        resp = requests.get(detected_image_url)
+        print('request ok')
+        detected_image = Image.open(resp.content)
+        # TODO : model restore 하고 결과값 불러오기
+        return Response({"pattern_result": pattern_result}, status=status.HTTP_200_OK)
