30f0b3541cd1b97c7bbb9b312aed7f3b6e8ce93f
ChanYeongShin <cyshin@yonsei.ac.kr>
2019-11-19 20:01:39 +0900

    [ADDED] ObjectDetectionAPIView

--- object_detection/views.py
+++ object_detection/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import mixins, status
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+import requests
+from PIL import Image
 
-# Create your views here.
+
+class ObjectDetectionAPIView(GenericAPIView):
+
+    def post(self, request, *args, **kwargs):
+        image_url = request.data['url']
+        resp = requests.get(image_url)
+        print('request ok')
+        image = Image.open(resp.content)
+        # TODO : model restore 하고 결과값 불러오기
+        return Response({"detected_image": detected_image}, status=status.HTTP_200_OK)
